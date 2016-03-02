a2abeee8484b689594f3a4d0171fb0b162bcf96c
abakal <a.p.bakal@yandex.ru>
2016-03-02 22:55:54 +0300

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -1,33 +1,28 @@
-import asyncore
-import socket
+import asyncore, socket
+
+class Server(asyncore.dispatcher):
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.bind(('', port))
+        self.listen(11)
+
+    def handle_accept(self):
+
+        socket, address = self.accept()
+        print ('Connection by', address)
+        EchoHandler(socket)
 
 class EchoHandler(asyncore.dispatcher_with_send):
 
     def handle_read(self):
-        data = self.recv(1024)
-        if data:
-            if data=="close":
-                self.close()
-            else:
-                self.send(data)
-            
+        self.data = self.recv(1024)
+        if not self.data:
+            self.close()
+        if self.data=="close":
+            self.close
+        else:
+            self.send(self.data)
 
-class EchoServer(asyncore.dispatcher):
-
-    def __init__(self, host, port):
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.set_reuse_addr()
-        self.bind((host, port))
-        self.clients[10]
-        self.listen(11)
-
-    def handle_accept(self):
-        pair = self.accept()
-        if pair is not None:
-            sock, addr = pair
-            print 'conn',addr
-            handler = EchoHandler(sock)
-
-server = EchoServer('0.0.0.0', 2222)
+s = Server('0.0.0.0', 2222)
 asyncore.loop()
