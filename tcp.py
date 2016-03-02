24c39a653323b4215715378b0ca98abb0cac5f05
abakal <a.p.bakal@yandex.ru>
2016-03-02 23:59:43 +0300

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -4,7 +4,7 @@ class Server(asyncore.dispatcher):
     def __init__(self, host, port):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.bind(('', port))
+        self.bind((host, port))
         self.listen(10)
 
     def handle_accept(self):
