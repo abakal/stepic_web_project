3c1241990abace4f7950dc949c3f71515650d9a4
abakal <a.p.bakal@yandex.ru>
2016-03-03 23:08:36 +0300

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -1,17 +1,6 @@
-import asyncore, socket
-
-class Server(asyncore.dispatcher):
-    def __init__(self, host, port):
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.bind((host, port))
-        self.listen(10)
-
-    def handle_accept(self):
-
-        socket, address = self.accept()
-        print ('Connection by', address)
-        EchoHandler(socket)
+#! /usr/bin/python
+import asyncore
+import socket
 
 class EchoHandler(asyncore.dispatcher_with_send):
 
@@ -24,5 +13,21 @@ class EchoHandler(asyncore.dispatcher_with_send):
                 print(data)
                 self.send(data)
 
-s = Server('0.0.0.0', 2222)
+class EchoServer(asyncore.dispatcher):
+
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(10)
+
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is not None:
+            sock, addr = pair
+            print 'Incoming connection from %s' % repr(addr)
+            handler = EchoHandler(sock)
+
+server = EchoServer('0.0.0.0', 2222)
 asyncore.loop()
