691db8072adf6197e34c1bb8c7e6266844268ec8
abakal <a.p.bakal@yandex.ru>
2016-03-10 21:55:44 +0300

    gunicorn

---
+++ hello.py
@@ -0,0 +1,9 @@
+def app(environ,start_response):
+    status='200 OK'
+    response_headers=[('Content-Type','text-plain')]
+    start_response(status,response_headers)
+    resp=environ['QUERY_STRING'].slit("&")
+    resp=[item+"\r\n" for item in resp]
+    return resp
+
+
