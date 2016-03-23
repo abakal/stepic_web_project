5b4909418447058dd35969338ae0f5f83ffe541c
abakal <a.p.bakal@yandex.ru>
2016-03-23 17:54:20 +0400

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     class Meta:
         db_table='question'
     title=models.CharField(max_length=255)
-    tex=models.TextField()
+    text=models.TextField()
     added_at=models.DateTimeField(auto_now_add=True)
     rating=models.IntegerField()
     author=models.ForeignKey(User,related_name='question_athor',blank=True,null=True)
