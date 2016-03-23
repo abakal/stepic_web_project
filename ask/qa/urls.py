68409945754e815419dc3f54cdcc21cae4b6d9c0
abakal <a.p.bakal@yandex.ru>
2016-03-23 18:27:06 +0400

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,14 +1,32 @@
-from django.conf.urls import include, url
+#from django.conf.urls import include, url
+#
+#from . import views
+#
+#urlpatterns = [
+#
+#    url(r'^$', views.home_page , name='index'),
+#    url(r'^login/.*$', views.user_login, name='login'),
+#    url(r'^signup/.*$', views.user_signup, name='signup'),
+#    url(r'^question/(?P<id>[0-9]+)/$', views.question_details, name='question'),
+#    url(r'^ask/.*$', views.add_question, name='ask'),
+#    url(r'^popular/.*$', views.popular_questions, name='popular'),
+#    url(r'^new/.*$', 'qa.views.test', name='new'),
+#]
 
-from . import views
+
+from .views import test
+from django.conf.urls import url
+
 
 urlpatterns = [
-
-    url(r'^$', views.home_page , name='index'),
-    url(r'^login/.*$', views.user_login, name='login'),
-    url(r'^signup/.*$', views.user_signup, name='signup'),
-    url(r'^question/(?P<id>[0-9]+)/$', views.question_details, name='question'),
-    url(r'^ask/.*$', views.add_question, name='ask'),
-    url(r'^popular/.*$', views.popular_questions, name='popular'),
-    #url(r'^new/.*$', 'qa.views.test', name='new'),
+    url(r'^$', test),
+    url(r'^login/$', test),
+    url(r'^signup/$', test),
+    url(r'^question/([0-9]+)/$', test),
+    url(r'^ask/\w*/ $', test),
+    url(r'^popular/$', test),
+    url(r'^new/$', test),
+     url(r'^popular/', list_popular, name='list-popular'),
+     url(r'^answer/', post_answer, name='post_answer'),
+     url(r'^question/(?P<slug>\w+)/$', show_question, name='show-question'),
 ]
